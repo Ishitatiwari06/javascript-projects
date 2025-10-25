9899ff678d47c14b083c622c6b5182071d4b3b65
Ishitatiwari06 <148783165+Ishitatiwari06@users.noreply.github.com>
2025-10-25 23:15:05 +0530

    Add files via upload

--- proj.js
+++ proj.js
@@ -1,8 +1,27 @@
-const body=document.querySelector('body')
-const buttons=document.querySelectorAll('.button')
-buttons.forEach(function(button){
-    button.addEventListener('click',function(e){
-        const color=e.target.id;
-        body.style.backgroundColor=color;
-    })
+const form=document.querySelector('form')
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    const height=parseInt(document.querySelector('#height').value)
+    const weight=parseInt(document.querySelector('#weight').value)
+    const ans=document.querySelector('#ans')
+    const ans2=document.querySelector('#ans2')
+    if(height==='' || isNaN(height) || height<0){
+        ans.innerHTML='Plz give valid height';
+    }
+    else if(weight==='' || isNaN(weight) || weight<0){
+        ans.innerHTML='Plz give valid weight';
+    }
+    else{
+        const bmi=(weight/((height*height)/1000)).toFixed(2);
+        ans.innerHTML=`<span> BMI : ${bmi}</span>`;
+        if(bmi<18.6){
+            ans2.innerHTML='Under Weight';
+        }
+        else if(bmi>=18.6 && bmi<=24.9){
+            ans2.innerHTML='Healthy and fit';
+        }
+        else{
+            ans2.innerHTML='Overweight';
+        }
+    }
 })
